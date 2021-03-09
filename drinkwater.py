e891043fd1cde0126c3cb7a84c929cf0cab96657
Abhinavkumarsing <abhinav.shingh.10@gmail.com>
2021-03-09 10:57:28 +0530

    i added drinkwater.py

--- drinkwater.py
+++ drinkwater.py
@@ -1,1 +1,13 @@
+import time
+from plyer import notification
 
+if __name__=="__main__":
+    while True:
+
+        notification.notify(
+            title="Its Time to Drink water",
+            message="Drinking water is very important because it keeps your body active andyour mind refreshed",
+            app_icon="/home/abhinav/drinkWater/glass.png.ico",
+            timeout=4
+    )
+    time.sleep(60*60)
